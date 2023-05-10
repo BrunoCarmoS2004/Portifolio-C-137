520011308eafa0bd6f039e121a9e24fa3dbceaba
BrunoCarmoS2004 <126525394+BrunoCarmoS2004@users.noreply.github.com>
2023-05-10 14:21:01 -0300

    add script

---
+++ Site-Prototipo-Para-App-Hospitais/scripts.js
@@ -0,0 +1,7 @@
+var btn = document.querySelector('.btn-entrar');
+var container = document.querySelector('#input-geral')
+
+btn.addEventListener('click', function(){
+
+    if(container.style.display)
+}

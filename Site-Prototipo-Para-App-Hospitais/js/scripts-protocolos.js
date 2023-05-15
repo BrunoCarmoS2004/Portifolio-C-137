45ab744aa1942ad53c0c68a4fe7a26eedda060bd
BrunoCarmoS2004 <126525394+BrunoCarmoS2004@users.noreply.github.com>
2023-05-15 15:13:12 -0300

    ]

---
+++ Site-Prototipo-Para-App-Hospitais/js/scripts-protocolos.js
@@ -0,0 +1,11 @@
+var tudo = document.querySelector('.p-titulosAll');
+var fav = document.querySelector('.p-titulosFav');
+var divtudo = document.querySelectorAll('.div-todos');
+var divtudo = document.querySelectorAll('.div-all');
+
+fav.addEventListener('click',function(){
+    
+        if (container.style.display === 'block');{
+            container.style.display = 'none';
+    }
+})
